9cfd4088bf2c067d8beb7e4e13ba08354a486cd5
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2023-12-15 11:46:36 +0800

    Update Spotify.jsx local -> host

--- frontend/src/pages/Spotify.jsx
+++ frontend/src/pages/Spotify.jsx
@@ -5,7 +5,7 @@ const code = new URLSearchParams(window.location.search).get("code");
 export default function Spotify() {
     return (
         <>
-            {code ? <SpotifyDashboard code={code}/> : <a className="btn m" href={"https://accounts.spotify.com/authorize?client_id=c427c4dced8b46f092bea016eb4ab5a0&response_type=code&redirect_uri=http://localhost:5173/spotify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"}>Login With Spotify</a>}
+            {code ? <SpotifyDashboard code={code}/> : <a className="btn m" href={"https://accounts.spotify.com/authorize?client_id=c427c4dced8b46f092bea016eb4ab5a0&response_type=code&redirect_uri=https://amenhotep-8ccvf23hu-cliffordwilliams-projects.vercel.app//spotify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"}>Login With Spotify</a>}
         </>
     )
 }

f8d9275aeb9f7704cf31d7f46f97e6ac63a3f2f7
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2023-12-15 11:41:21 +0800

    Update main.jsx Store/index.js path

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import router from './router'
 import { Provider } from "react-redux";
-import { store } from "./store/index";
+import { store } from "./Store/index";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
